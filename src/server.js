dad7500ca346ea2134fc453f68af3b2afe97f511
adam-almeida <adamportifolio@gmail.com>
2023-01-09 22:22:48 -0300

    fundamentos do node

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from "node:http";
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+        id: 1,
+        name: 'Jhon Doe',
+        email: 'jhondoe@email.com'
+    })
+    return res.writeHead(201).end();
+  }
+
+  console.log(method, url);
+  return res.writeHead(404).end("Not Found");
+});
+
+server.listen(3333, () => console.log("Server is running"));
