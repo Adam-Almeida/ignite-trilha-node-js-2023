6e1a42389c20607c327452a3c571430e66e880d1
adam-almeida <adamportifolio@gmail.com>
2023-01-11 00:11:03 -0300

    full stream/await request

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -9,9 +9,20 @@ class InverseNumberStream extends Transform {
   }
 }
 
-const server = http.createServer((req, res) => {
-  return req.pipe(new InverseNumberStream())
-  .pipe(res);
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullBody = Buffer.concat(buffers).toString();
+
+  console.log(fullBody);
+
+  return res.end(fullBody);
+  // return req.pipe(new InverseNumberStream())
+  // .pipe(res);
 });
 
 server.listen(3334, () => console.log("Server is running on port 3334"));
