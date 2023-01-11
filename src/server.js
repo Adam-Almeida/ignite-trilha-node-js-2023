a888fcf63818f9fe77e00cea54634e1464f4cde8
adam-almeida <adamportifolio@gmail.com>
2023-01-11 18:49:41 -0300

    request body json parse and full stream

--- src/server.js
+++ src/server.js
@@ -1,22 +1,36 @@
 import http from "node:http";
 
-const users = []
+const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     return res
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users));
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
-        id: 1,
-        name: 'Jhon Doe',
-        email: 'jhondoe@email.com'
-    })
+      id: 1,
+      name,
+      email,
+    });
     return res.writeHead(201).end();
   }
 
