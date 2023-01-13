ce1fe27677b1bee90f479b9f73399c46f6764d28
adam-almeida <adamportifolio@gmail.com>
2023-01-13 00:25:28 -0300

    refactor regex params url

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ const server = http.createServer(async (req, res) => {
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+
+    console.log(routeParams);
     return route.handler(req, res);
   }
 
